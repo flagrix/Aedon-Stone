d849740ac9b4a9b132f6af9e737fce73e18fdb28
Kilian Pligersdorffer <kilian.pligersdorffer@epita.fr>
2025-02-28 17:08:40 +0100

    Menus remis

---
+++ Assets/SCRIPT/Inventory.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class Inventory : MonoBehaviour
+{
+    public int runes;
+    public int potions;
+    public Text runesText; //pour afficher les runes
+    public Text potionsText;
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this; //Pour pouvoir faire appel a inventory de partout
+    }
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            addRune(1);
+            addPotion(1);
+            HealthBar.instance.SetActualHealth(20);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            addRune(-1);
+            addPotion(-1);
+            HealthBar.instance.SetActualHealth(-20);
+        }
+    }
+
+    public void addRune(int newRune)
+    {
+        if (runes + newRune < 0)
+            runes = 0;
+        else
+        {
+            runes += newRune;
+            runesText.text = runes.ToString();
+        }
+        if (runes > 9 && !GameOver.instance.isGameOver)
+            GameOver.instance.EndGame();
+    }
+    public void addPotion(int newpotion)
+    {
+        if (potions + newpotion < 0)
+            potions = 0;
+        else
+        {
+            potions += newpotion;
+            potionsText.text = potions.ToString();
+        }
+
+
+    }
+}
+
