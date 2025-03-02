4035711b8a49397ead6380e389c306a28507ec00
blast4631 <chevalier.amau@gmail.com>
2025-03-02 15:27:23 +0100

    tir

--- Assets/defense.cs
+++ Assets/defense.cs
@@ -11,10 +11,16 @@ public class defense : MonoBehaviour {
 
     private float turnSpeed = 6.5f;
 
-    // Use this for initialization
-    void Start () {
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+// Use this for initialization
+void Start () {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
-    }
+}
 
     void UpdateTarget()
     {
@@ -42,9 +48,9 @@ public class defense : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
-    void Update () {
-        if(target == null)
+// Update is called once per frame
+void Update () {
+if(target == null)
         {
             return;
         }
@@ -53,6 +59,25 @@ public class defense : MonoBehaviour {
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1 / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+}
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()
