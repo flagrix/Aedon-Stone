8673611c461dedd9da367752f69b6e10d42c4d73
Rocchic <113167741+myrthioty@users.noreply.github.com>
2025-05-23 14:21:38 +0200

    en cours
    c rien

--- Assets/SCRIPT/Weapons/Hammer.cs
+++ Assets/SCRIPT/Weapons/Hammer.cs
@@ -5,6 +5,7 @@ public class Hammer : Item
 
     public AudioSource WazeAudioSource;
     public AudioClip hammer_sound;
+    private Camera cam;
 
     void Start()
     {
@@ -13,10 +14,42 @@ public class Hammer : Item
     }
     public override void Use()
     { 
-        Debug.Log("Hammer"+ itemScriptableObject.itemType.ToString());
+        if (itemScriptableObject.cooldown < itemScriptableObject.tempecoule)
+        {
+            Build();
+            itemScriptableObject.tempecoule = 0;
+        }
+        else
+        {
+            Debug.Log("Hammer"+ itemScriptableObject.itemType.ToString());
+        }
+        
+    }
+
+    public void NodeOverview()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        ray.origin = cam.transform.position;
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            var temp = hit.collider.gameObject.GetComponent<Node>();
+        }
+    }
+
+    void Build()
+    {
         if (!WazeAudioSource.isPlaying)
         {
             WazeAudioSource.Play();
         }
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        ray.origin = cam.transform.position;
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log("we hit " + hit.collider.gameObject.name);
+            var temp = hit.collider.gameObject.GetComponent<Node>();
+            
+        }
+        
     }
 }
