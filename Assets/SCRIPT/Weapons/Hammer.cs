8ea69206ef5c2fadd3b26148109c117510a3c79d
flagrix <g.benier9@yahoo.com>
2025-05-23 02:13:33 +0200

    modifs map
    ajout d'interieur pour le shop du spawn
    ajout de barrieres
    suppression d'assets inutiles

--- Assets/SCRIPT/Weapons/Hammer.cs
+++ Assets/SCRIPT/Weapons/Hammer.cs
@@ -8,7 +8,7 @@ public class Hammer : Item
 
     void Start()
     {
-        WazeAudioSource.clip = hammer_sound;
+        //WazeAudioSource.clip = hammer_sound;
     }
     public override void Use()
     {
